ad4f65dd27d2e73fe031cd5b2a80177f5d42a889
limmyj <162650099+limmyj@users.noreply.github.com>
2024-06-28 18:10:46 +0800

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Trainers]
+(
+	[TPNum] nvarchar(20) NOT NULL PRIMARY KEY,
+	[Name] nvarchar(20) NOT NULL,
+	[Email] nvarchar(50) NOT NULL,
+	[ContactNumber] nvarchar(20) NULL,
+	[Address] nvarchar(50) NULL,
+	[Biodata] nvarchar(50) NULL
+)
---
+++ dbo.Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [TPNumber]    NVARCHAR (20) NOT NULL,
+    [Password] NVARCHAR (20) NOT NULL,
+    [Role]     NVARCHAR (20) NOT NULL,
+    PRIMARY KEY CLUSTERED ([TPNum] ASC)
+);
+
